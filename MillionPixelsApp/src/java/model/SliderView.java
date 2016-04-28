9b36c09e4f338ccb53d7c6edd558ca86fa6e5ec9
it3530114 <it3530114@WEBDEV-POOL23.ad.ilstu.edu>
2016-04-27 21:59:19 -0500

    SliderView updated

--- MillionPixelsApp/src/java/model/SliderView.java
+++ MillionPixelsApp/src/java/model/SliderView.java
@@ -7,18 +7,53 @@ package model;
  
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
  
 import org.primefaces.event.SlideEndEvent;
  
-@ManagedBean
+@ManagedBean(name ="sliderView")
+@SessionScoped
+@RequestScoped
 public class SliderView
 {
     private int number3;   
- 
+    private double donation;
+    private double finalDonation;
+    private String message;
+    private final double PIXEL_PRICE =.22;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public double getFinalDonation() {
+        return finalDonation;
+    }
+
+    public void setFinalDonation(double finalDonation) {
+        this.finalDonation = finalDonation;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
     public int getNumber3() 
     {
         return number3;
+    }
+
+    public double getDonation() 
+    {
+        return donation;
+    }
+
+    public void setDonation(double donation) 
+    {
+        this.donation = donation;
+        toString();
     }
  
     public void setNumber3(int number3) 
@@ -26,13 +61,24 @@ public class SliderView
         this.number3 = number3;
     }
 
+    //.22 assumed price per pixel
     public double calculateDonation()
     {
-        return number3 * .10;
+        setDonation(number3 * PIXEL_PRICE);
+        return donation;
     }
      
     public void onSlideEnd(SlideEndEvent event) {
-        FacesMessage message = new FacesMessage("Slide Ended", "Value: " + event.getValue());
+        FacesMessage message = new FacesMessage("Slide Ended", "Value: " + getDonation());
         FacesContext.getCurrentInstance().addMessage(null, message);
     } 
+
+    @Override
+    public String toString() {
+        return "SliderView{" + "number3=" + number3 + ", donation=" + donation + ", message=" + message + '}';
+    }
+
+    
+    
+    
 }
